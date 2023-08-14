16a82e4b4a987aedfdfaa76f5cdf022965170172
maximtim <maximtim1337@gmail.com>
2023-08-14 20:21:11 +0300

    task2 solution

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, Tuple, TupleBuilder, parseTuple, serializeTuple, toNano } from 'ton-core';
 import { Task2 } from '../wrappers/Task2';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
@@ -34,5 +34,56 @@ describe('Task2', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
+    });
+
+    it('calculates', async () => {
+        // const matrix1 = new TupleBuilder();
+        // const m1Row1 = new TupleBuilder();
+        // m1Row1.writeTuple(
+        //     [{ type: 'int', value:  BigInt(1) },
+        //     { type: 'int', value:  BigInt(1) }]
+        // );
+        // m1Row1.build();
+
+        const matrixA : Tuple = { type: 'tuple', items: [
+            { type: 'tuple', items:  [
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n }
+            ] },
+            { type: 'tuple', items:  [
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n }
+            ] },
+            { type: 'tuple', items:  [
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n }
+            ] }
+        ] };
+
+        const matrixB : Tuple = { type: 'tuple', items: [
+            { type: 'tuple', items:  [
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n }
+            ] },
+            { type: 'tuple', items:  [
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n },
+                { type: 'int', value:  1n }
+            ] }
+        ] };
+
+        const { stackReader, stack } = await blockchain.runGetMethod(task2.address, 'matrix_multiplier', [
+            matrixA,
+            matrixB,
+        ]);
+
+        // const res = stackReader;
+        const res = stackReader.readTuple();
+        console.log(res.readTuple());
+        console.log(res.readTuple());
+        console.log(res.readTuple());
     });
 });
